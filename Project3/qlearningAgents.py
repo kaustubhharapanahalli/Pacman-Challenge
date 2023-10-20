520f3cee67a6af3ec8296523956fd31ffa1def59
Kaustubh Harapanahalli <kaustubhharapanahalli@gmail.com>
2023-10-20 11:04:06 -0700

    Implementation of getAction function for question 7

--- Project3/qlearningAgents.py
+++ Project3/qlearningAgents.py
@@ -112,8 +112,16 @@ class QLearningAgent(ReinforcementAgent):
         # Pick Action
         legalActions = self.getLegalActions(state)
         action = None
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+
+        if len(legalActions) == 0:
+            return None
+
+        if util.flipCoin(self.epsilon):
+            action = random.choice(legalActions)
+        else:
+            action = self.computeActionFromQValues(state)
+
+        return action
 
         return action
 
